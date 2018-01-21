1cfc2405579c65fdf321fa52b5d76aeabc2f1c99
agnes-kabaly <alicet.woods@gmail.com>
2018-01-21 23:48:19 +0100

    AddDay started

--- src/workouts/AddDays.js
+++ src/workouts/AddDays.js
@@ -1,5 +1,24 @@
 import React from "react";
+import { StyleSheet, View, Text, Button, TextInput, TouchableOpacity, KeyboardAvoidingView } from "react-native";
 
 export default class AddDays extends React.Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            dayName: "",
+        }
+    }
+
+    render () {
+        return (
+            <KeyboardAvoidingView>
+                <View>
+                    <Text>is it ok now</Text>
+                    <TextInput placeholder="oké"/>
+                </View>
+            </KeyboardAvoidingView>
+        )
+    }
 }
