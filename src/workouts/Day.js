e7211f052b7fb0e453867fd24111d5c81b4e41d9
agnes-kabaly <alicet.woods@gmail.com>
2018-02-16 16:34:29 +0100

    day - exercises FlatList started with formatting

--- src/workouts/Day.js
+++ src/workouts/Day.js
@@ -1,7 +1,18 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, FlatList, StyleSheet} from 'react-native';
+import FlatListItem from './FlatListItem';
+import EditModal from './EditModal';
 
 export default class Day extends React.Component {
+
+    renderSeparator = () => {
+        return(
+            <View
+                style={styles.separatorStyle}
+            />
+        )
+    };
+
     render(){
 
         const { navigate } = this.props.navigation;
@@ -9,12 +20,51 @@ export default class Day extends React.Component {
         var exerciseList = this.props.navigation.state.params.workoutDay.workouts;
 
         return(
-            <View>
-                <Text>Dax oki</Text>
+            <View style={styles.container}>
                 <Text>{this.props.navigation.state.params.workoutDay.dayName}</Text>
                 <Text>{this.props.navigation.state.params.workoutDay.color}</Text>
                 <Text>{exerciseList.valueOf().length}</Text>
+                <View style={styles.flatContainer}>
+                    <FlatList
+                        ref={"flatList"}
+                        data={exerciseList}
+                        ItemSeparatorComponent={this.renderSeparator}
+                        renderItem={({item, index}) => {
+                            return (
+                                <FlatListItem item={item} index={index} parentFlatList={this}>
+
+                                </FlatListItem>
+                            )
+                        }}
+                    >
+                    </FlatList>
+                </View>
+
+                <EditModal ref={"editModal"} parentFlatList={this}>
+
+                </EditModal>
+
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#e6b800',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    flatContainer: {
+        flex: 2,
+        paddingHorizontal: 15,
+        backgroundColor: '#e6b800',
+    },
+    separatorStyle: {
+        height: 7,
+        width: "86%",
+        backgroundColor: '#e6b800',
+        marginLeft: "14%",
+    },
+});
